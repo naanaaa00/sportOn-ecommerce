d505362090107541ce88d062c766d71cdfc9ffa0
naanaaa00 <waridatuljannah00@gmail.com>
2026-01-21 11:52:11 +0700

    v-2

---
+++ app/(landing)/components/ui/card-with-header.tsx
@@ -0,0 +1,19 @@
+type TCartWithHeaderProps = {
+    title: string;
+    children: React.ReactNode;
+}
+
+const CardWithHeader = ( {title, children} : TCartWithHeaderProps) => {
+    return (
+        <div className="bg-white">
+            <div className="px-5 py-5 border-b border-gray-200">
+                <h2 className="font-bold text-lg">
+                    {title}
+                </h2>
+            </div>
+            {children}
+        </div>
+    )
+}
+
+export default CardWithHeader;
---
+++ app/(landing)/payment/page.tsx
@@ -0,0 +1,22 @@
+import CartItems from "../components/checkout/cart-items"
+import OrderInformation from "../components/checkout/order-information"
+import PaymentOptions from "../components/payment/payment-options"
+import PaymentSteps from "../components/payment/payment-steps"
+
+const Payment = () => {
+    return (
+        <main className="bg-gray-100 min-h-[80vh]">
+        <div className="max-w-5xl mx-auto py-20">
+                <h1 className="text-5xl font-bold text-center mb-10">
+                    Payment
+                </h1>
+            <div className="grid grid-cols-2 gap-14">  
+                <PaymentOptions />
+                <PaymentSteps />
+            </div>
+        </div>
+        </main>
+    )
+}
+
+export default Payment
